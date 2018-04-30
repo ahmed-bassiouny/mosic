8757ef7d990dbc0d3bf5587a3017ddf785ac1250
bassiouny <ahmedbassiouny26@gmail.com>
2018-04-30 13:34:48 +0200

    init

---
+++ app/src/main/java/ntamtech/mosic/HomeController.java
@@ -0,0 +1,117 @@
+package ntamtech.mosic;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Environment;
+import android.support.v4.print.PrintHelper;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by bassiouny on 30/04/18.
+ */
+
+public class HomeController {
+
+    private String mosicPath = Environment.getExternalStorageDirectory() +"/Mosic/";
+    private String originalPath = mosicPath+"Original/";
+    private String editedPath = mosicPath+"Edited/";
+    private Activity activity;
+    private Canvas canvas;
+    private Paint alphaPaint;
+
+    public HomeController(Activity activity) {
+        this.activity = activity;
+        File mosicFile = new File(mosicPath);
+        File originalFile = new File(originalPath);
+        File editedFile = new File(editedPath);
+
+        if(!mosicFile.exists()){
+            mosicFile.mkdir();
+        }
+        if (!originalFile.exists()) {
+            originalFile.mkdir();
+        }
+        if (!editedFile.exists()) {
+            editedFile.mkdir();
+        }
+    }
+
+    public Bitmap addWaterMark(Bitmap src, Bitmap imageFromDevice, int alpha) {
+
+        int w = src.getWidth();
+        int h = src.getHeight();
+        Bitmap result = Bitmap.createBitmap(w, h, src.getConfig());
+        canvas = new Canvas(result);
+        alphaPaint = new Paint();
+        alphaPaint.setAlpha(alpha);
+        canvas.drawBitmap(src, 0, 0, alphaPaint);
+
+        canvas.drawBitmap(imageFromDevice, 0, 0, null);
+        return result;
+    }
+
+    public Bitmap getBitmapFromStringPath(String fileName) {
+
+        String photoPath1 = originalPath + fileName;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+        Bitmap waterMark = BitmapFactory.decodeFile(photoPath1, options);
+        return waterMark;
+    }
+
+    public void saveNewImage(Bitmap result, String fileName) {
+        FileOutputStream out;
+        try {
+            out = new FileOutputStream(editedPath + fileName);
+
+            result.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+        } catch (FileNotFoundException e) {
+            Toast.makeText(activity, "Can\'t save image", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Toast.makeText(activity, "Can\'t save image", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public ArrayList<String> fetchFileNames() {
+
+        ArrayList<String> filenames = new ArrayList<>();
+        File directory = new File(originalPath);
+
+        File[] files = directory.listFiles();
+
+        for (int i = 0; i < files.length; i++) {
+
+            String file_name = files[i].getName();
+            // you can store name to arraylist and use it later
+            filenames.add(file_name);
+        }
+        return filenames;
+    }
+
+    public int randomElement(ArrayList<String> files) {
+        Random rand = new Random();
+        return rand.nextInt(files.size());
+    }
+    public void deleteImage(String fileName){
+        File file = new File(originalPath+fileName);
+        file.delete();
+    }
+    public void doPhotoPrint(Bitmap bitmap) {
+        PrintHelper photoPrinter = new PrintHelper(activity);
+        photoPrinter.setScaleMode(PrintHelper.SCALE_MODE_FIT);
+        photoPrinter.printBitmap("droids.jpg - test print", bitmap);
+    }
+}
---
+++ app/src/main/java/ntamtech/mosic/MainActivity.java
@@ -0,0 +1,127 @@
+package ntamtech.mosic;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import com.mvc.imagepicker.ImagePicker;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView myImage;
+    SeekBar seekBar;
+    private Bitmap selectedBitmap;
+    private HomeController controller;
+    private ArrayList<String> filesName;
+    private Button select, save;
+    private Bitmap newBitmap;
+    private ProgressBar progress;
+    private String currentFile;
+    private int currentIndex;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myImage = findViewById(R.id.imageviewTest);
+        seekBar = findViewById(R.id.seek_bar);
+        select = findViewById(R.id.select);
+        save = findViewById(R.id.save);
+        progress = findViewById(R.id.progress);
+        onClick();
+        ImagePicker.setMinQuality(600, 600);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                filesName = getController().fetchFileNames();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progress.setVisibility(View.GONE);
+                        if(filesName.size() == 0){
+                            Toast.makeText(MainActivity.this, "no image found", Toast.LENGTH_SHORT).show();
+                        }else {
+                            select.setVisibility(View.VISIBLE);
+                        }
+                    }
+                });
+            }
+        }).start();
+    }
+
+    private void onClick() {
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImagePicker.pickImage(MainActivity.this, "Select your image:");
+            }
+        });
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (newBitmap == null)
+                    return;
+                getController().saveNewImage(newBitmap, currentFile);
+                getController().deleteImage(currentFile);
+                filesName.remove(currentIndex);
+                Toast.makeText(MainActivity.this, "Saved", Toast.LENGTH_SHORT).show();
+                getController().doPhotoPrint(newBitmap);
+            }
+        });
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                newBitmap = getController().addWaterMark(selectedBitmap, getController().getBitmapFromStringPath(currentFile), seekBar.getProgress());
+                myImage.setImageBitmap(newBitmap);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Bitmap bitmap = ImagePicker.getImageFromResult(this, requestCode, resultCode, data);
+        if (bitmap != null) {
+            this.selectedBitmap = bitmap;
+            currentIndex = getController().randomElement(filesName);
+            currentFile = filesName.get(currentIndex);
+            newBitmap = getController().addWaterMark(selectedBitmap, getController().getBitmapFromStringPath(currentFile), seekBar.getProgress());
+            myImage.setImageBitmap(newBitmap);
+            seekBar.setVisibility(View.VISIBLE);
+            save.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    private HomeController getController() {
+        if (controller == null)
+            controller = new HomeController(this);
+        return controller;
+    }
+
+}
